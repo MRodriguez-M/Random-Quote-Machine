ba2b974360051a9658957bf2f2cae4c8437f9f98
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-01-17 17:27:16 -0600

    Added and styled quote and author text

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,7 +3,10 @@ import React from "react";
 const Quote = () => {
     return (
         <>
-            <div id="quote-box">quote box</div>
+            <div id="quote-box">
+                <p id="text">"The worst enemy to creativity is self-doubt."</p>
+                <p id="author">- Sylvia Plath</p>
+            </div>
         </>
     );
 };
