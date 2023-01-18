d8f15b31f39616f34be601da664175408c994e1a
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-01-17 18:10:32 -0600

    Added and styled tweet and quote buttons

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,9 +3,17 @@ import React from "react";
 const Quote = () => {
     return (
         <>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
             <div id="quote-box">
                 <p id="text">"The worst enemy to creativity is self-doubt."</p>
                 <p id="author">- Sylvia Plath</p>
+                <a id="tweet-quote">
+                    <button id="tweet-button" type="button">
+                        <i class="fa fa-twitter"></i> Share on Twitter
+                    </button>
+                </a>
+                <button id="new-quote" type="button">New quote</button>
             </div>
         </>
     );
