6e2250e7c583f5e4da1e069935ece2469921eb67
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-01-18 18:43:51 -0600

    Added quote data through an array of objects

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,13 @@ import Quote from "./Quote"
 
 function App() {
   return (
-    <Quote />
+    <Quote 
+      quotes={[
+        { quote: 'The worst enemy to creativity is self-doubt.', author: 'Sylvia Plath' },
+        { quote: 'You will face many defeats in life, but never let yourself be defeated.', author: 'Maya Angelou' },
+        { quote: 'Life is not easy for any of us. But what of that? We must have perseverance and above all confidence in ourselves.', author: 'Marie Curie' }
+      ]}
+    />
   );
 }
 
