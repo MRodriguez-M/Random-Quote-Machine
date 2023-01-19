25af0f35cb2d0bd9892ef364d62325a15481ef02
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-01-18 18:46:35 -0600

    Added state and handleClick to randomize quote display

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,19 +1,26 @@
 import React from "react";
+import { useState } from 'react';
 
-const Quote = () => {
+const Quote = (props) => {
+    const [index, setIndex] = useState(0);
+
+    const handleClick = () => {
+        setIndex(Math.floor(Math.random() * 3));
+    };
+
     return (
         <>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
             <div id="quote-box">
-                <p id="text">"The worst enemy to creativity is self-doubt."</p>
-                <p id="author">- Sylvia Plath</p>
+                <p id="text">"{props.quotes[index].quote}"</p>
+                <p id="author">- {props.quotes[index].author}</p>
                 <a id="tweet-quote">
                     <button id="tweet-button" type="button">
                         <i class="fa fa-twitter"></i> Share on Twitter
                     </button>
                 </a>
-                <button id="new-quote" type="button">New quote</button>
+                <button id="new-quote" type="button" onClick={handleClick}>New quote</button>
             </div>
         </>
     );
